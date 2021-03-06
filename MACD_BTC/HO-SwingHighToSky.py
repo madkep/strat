5d84ce92fafedb1f2666d8645edb66ca781642b6
Felipe Cespedes <felipecespedes97@gmail.com>
2021-03-05 21:34:45 -0300

    Revert "Test trailing "
    This reverts commit 91a55901d8991fedb09194bdf3730b2216a1e3b6.

--- MACD_BTC/HO-SwingHighToSky.py
+++ MACD_BTC/HO-SwingHighToSky.py
@@ -44,33 +44,6 @@ class HOSwingHighToSky(IHyperOpt):
         return dataframe
 
     @staticmethod
-    def trailing_space() -> List[Dimension]:
-        """
-        Create a trailing stoploss space.
-        You may override it in your custom Hyperopt class.
-        """
-        return [
-            # It was decided to always set trailing_stop is to True if the 'trailing' hyperspace
-            # is used. Otherwise hyperopt will vary other parameters that won't have effect if
-            # trailing_stop is set False.
-            # This parameter is included into the hyperspace dimensions rather than assigning
-            # it explicitly in the code in order to have it printed in the results along with
-            # other 'trailing' hyperspace parameters.
-            Categorical([True], name='trailing_stop'),
-
-            Real(0.01, 0.35, name='trailing_stop_positive'),
-
-            # 'trailing_stop_positive_offset' should be greater than 'trailing_stop_positive',
-            # so this intermediate parameter is used as the value of the difference between
-            # them. The value of the 'trailing_stop_positive_offset' is constructed in the
-            # generate_trailing_params() method.
-            # This is similar to the hyperspace dimensions used for constructing the ROI tables.
-            Real(0.001, 0.1, name='trailing_stop_positive_offset_p1'),
-
-            Categorical([True, False], name='trailing_only_offset_is_reached'),
-        ]
-
-    @staticmethod
     def buy_strategy_generator(params: Dict[str, Any]) -> Callable:
    
         def populate_buy_trend(dataframe: DataFrame, metadata: dict) -> DataFrame:
